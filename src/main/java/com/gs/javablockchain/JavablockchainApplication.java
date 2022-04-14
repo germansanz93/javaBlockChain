f33d22fc62f3348681b25fbf667d4689c0e9ddcd
German Sanz <germansanz.id@pinapp.pe>
2022-04-14 02:07:51 -0300

    first commit

---
+++ src/main/java/com/gs/javablockchain/JavablockchainApplication.java
@@ -0,0 +1,13 @@
+package com.gs.javablockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavablockchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavablockchainApplication.class, args);
+	}
+
+}
